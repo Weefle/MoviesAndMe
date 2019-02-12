f417083ed59f73ac4028e65b50fc4c5e54d43cde
Weefle <mr.weefle@gmail.com>
2019-02-12 14:43:37 +0100

    Some Improvements

---
+++ components/API/TMDBApi.js
@@ -0,0 +1,8 @@
+const API_TOKEN = '98df78b64e2ffa02ca344247d3361cf4'
+
+export function getFilmsFromApiWithSearchedText (text) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
--- components/Search.js
+++ components/Search.js
@@ -1,15 +1,53 @@
 import React from 'react'
-import {View, Button, TextInput} from 'react-native'
+import {StyleSheet, View, Button, TextInput, FlatList, Text} from 'react-native'
+import FilmItem from './FilmItem'
+import {getFilmsFromApiWithSearchedText} from './API/TMDBApi'
 
 export default class Search extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {films:[]}
+        this.searchedText = ""
+    }
+
+_loadFilms(){
+
+    if(this.searchedText.length > 0){
+getFilmsFromApiWithSearchedText(this.searchedText).then(data=>this.setState({films:data.results}))
+    }
+
+}
+
+_searchTextInputChanged(text){
+
+    this.searchedText = text
+
+}
+
     render(){
         return(
 
-            <View>
-                <TextInput placeholder="Titre du film"></TextInput>
-                <Button title="Rechercher" onPress={()=>{}}></Button>
+            <View style={styles.main_container}>
+                <TextInput onChangeText={(text)=> this._searchTextInputChanged(text)} style={styles.textInput} placeholder='Titre du film'></TextInput>
+                <Button title="Rechercher" onPress={()=>this._loadFilms()}></Button>
+                <FlatList
+  data={this.state.films}
+  keyExtractor={(item) => item.id.toString()}
+  renderItem={({item}) => <FilmItem film = {item}></FilmItem>}
+/>
             </View>
 
         )
     }
 }
+
+const styles = StyleSheet.create({
+    main_container : {
+        marginTop:25,
+        flex:1
+    },
+    textInput : {
+        marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5
+    }
+})
