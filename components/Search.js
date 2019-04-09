98e304b9aa46d7ab21eef437f92b7fe021bb0b16
Weefle <mr.weefle@gmail.com>
2019-04-10 00:08:06 +0200

    Added image and loading page

--- components/Search.js
+++ components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, Button, TextInput, FlatList, Text} from 'react-native'
+import {StyleSheet, View, Button, TextInput, FlatList, Text, ActivityIndicator} from 'react-native'
 import FilmItem from './FilmItem'
 import {getFilmsFromApiWithSearchedText} from './API/TMDBApi'
 
@@ -7,16 +7,29 @@ export default class Search extends React.Component {
 
     constructor(props){
         super(props)
-        this.state = {films:[]}
+        this.state = {films:[],
+        isLoading: false}
         this.searchedText = ""
     }
 
 _loadFilms(){
-
+this.setState({isLoading: true})
     if(this.searchedText.length > 0){
-getFilmsFromApiWithSearchedText(this.searchedText).then(data=>this.setState({films:data.results}))
+getFilmsFromApiWithSearchedText(this.searchedText).then(data=>this.setState({
+    films:data.results, 
+    isLoading: false
+}))
     }
 
+}
+
+_displayLoading(){
+    if(this.state.isLoading){
+        return(
+            <View style={styles.loading_container}><ActivityIndicator size='large'/></View>
+
+        )
+    }
 }
 
 _searchTextInputChanged(text){
@@ -26,16 +39,18 @@ _searchTextInputChanged(text){
 }
 
     render(){
+       // console.log(this.state.isLoading);
         return(
 
             <View style={styles.main_container}>
-                <TextInput onChangeText={(text)=> this._searchTextInputChanged(text)} style={styles.textInput} placeholder='Titre du film'></TextInput>
+                <TextInput onSubmitEditing={()=>this._loadFilms()} onChangeText={(text)=> this._searchTextInputChanged(text)} style={styles.textInput} placeholder='Titre du film'></TextInput>
                 <Button title="Rechercher" onPress={()=>this._loadFilms()}></Button>
                 <FlatList
   data={this.state.films}
   keyExtractor={(item) => item.id.toString()}
   renderItem={({item}) => <FilmItem film = {item}></FilmItem>}
 />
+{this._displayLoading()}
             </View>
 
         )
@@ -49,5 +64,14 @@ const styles = StyleSheet.create({
     },
     textInput : {
         marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5
-    }
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }
 })
