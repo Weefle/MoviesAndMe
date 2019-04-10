6cf2e4c3e7bf9159b6c3811b46005cdbc3a81164
Weefle <mr.weefle@gmail.com>
2019-04-10 23:08:09 +0200

    Lot of bugfixes

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Search from './components/Search'
+import Navigation from './navigation/Navigation'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Search></Search>
+      <Navigation/>
     )
   }
 }
--- components/Search.js
+++ components/Search.js
@@ -1,92 +1,111 @@
 import React from 'react'
-import {StyleSheet, View, Button, TextInput, FlatList, Text, ActivityIndicator} from 'react-native'
+import { StyleSheet, View, TextInput, Button, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
-import {getFilmsFromApiWithSearchedText} from './API/TMDBApi'
+import { getFilmsFromApiWithSearchedText } from './API/TMDBApi'
 
-export default class Search extends React.Component {
+class Search extends React.Component {
 
-    constructor(props){
-        super(props)
-        this.page = 0
-        this.totalPages = 0
-        this.state = {films:[],
-        isLoading: false}
-        this.searchedText = ""
+  constructor(props) {
+    super(props)
+    this.searchedText = ""
+    this.page = 0
+    this.totalPages = 0
+    this.state = {
+      films: [],
+      isLoading: false
     }
+  }
 
-_loadFilms(){
-this.setState({isLoading: true})
-    if(this.searchedText.length > 0){
-getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data=>{
-    this.page = data.page
-    this.totalPages = data.totalPages
+  _loadFilms() {
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true })
+      getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+          this.page = data.page
+          this.totalPages = data.total_pages
+          this.setState({
+            films: [ ...this.state.films, ...data.results ],
+            isLoading: false
+          })
+      })
+    }
+  }
+
+  _searchTextInputChanged(text) {
+    this.searchedText = text 
+  }
+
+  _searchFilms() {
+    this.page = 0
+    this.totalPages = 0
     this.setState({
-        films: this.state.films.concat(data.results),
-    //films: [...this.state.films, ...data.results], 
-    isLoading: false
-})
-}
-)
-    }
+      films: [],
+    }, () => {
+        this._loadFilms()
+    })
+  }
 
-}
-
-_displayLoading(){
-    if(this.state.isLoading){
-        return(
-            <View style={styles.loading_container}><ActivityIndicator size='large'/></View>
-
-        )
-    }
+  _displayDetailForFilm=(idFilm)=>{
+    this.props.navigation.navigate("FilmDetail")
 }
 
-_searchTextInputChanged(text){
-
-    this.searchedText = text
-
-}
-
-    render(){
-       // console.log(this.state.isLoading);
-        return(
-
-            <View style={styles.main_container}>
-                <TextInput onSubmitEditing={()=>this._loadFilms()} onChangeText={(text)=> this._searchTextInputChanged(text)} style={styles.textInput} placeholder='Titre du film'></TextInput>
-                <Button title="Rechercher" onPress={()=>this._loadFilms()}></Button>
-                <FlatList
-  data={this.state.films}
-  keyExtractor={(item) => item.id.toString()}
-  onEndReachedThreshold={0.5}
-  onEndReached={()=>
-{
-    if(this.page<this.totalPages){
-        this._loadFilms
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
     }
-}}
-  renderItem={({item}) => <FilmItem film = {item}></FilmItem>}
-/>
-{this._displayLoading()}
-            </View>
+  }
 
-        )
-    }
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._searchFilms()}
+        />
+        <Button title='Rechercher' onPress={() => this._searchFilms()}/>
+        <FlatList
+          data={this.state.films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem film={item} displayDetailForFilm={this._displayDetailForFilm}/>}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+              if (this.page < this.totalPages) {
+                 this._loadFilms()
+              }
+          }}
+        />
+        {this._displayLoading()}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    main_container : {
-        marginTop:25,
-        flex:1
-    },
-    textInput : {
-        marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5
-    },
-    loading_container: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 100,
-        bottom: 0,
-        alignItems: 'center',
-        justifyContent: 'center'
-      }
+  main_container: {
+    flex: 1,
+  },
+  textinput: {
+    marginLeft: 1,
+    marginRight: 1,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 2,
+    paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
+
+export default Search
